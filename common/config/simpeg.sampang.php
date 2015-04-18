052dd33a174b04e29cc09a728aa31e8ae0dce8a2
tindik <tindik@asega>
2015-04-18 11:39:30 +0700

    Merge origin/master
    Conflicts:
    	amarta/controllers/WorkorderController.php

--- common/config/simpeg.sampang.php
+++ common/config/simpeg.sampang.php
@@ -1,16 +1,16 @@
 <?php
 //=========change this scope=========================================================
-$root = '/var/www/sampang/taro_simpeg/';
+$root = '/var/www/taro_simpeg/';
 //$root = 'D:/xampp/htdocs/taro_simpeg/'; //local
-$themesUrl = 'http://localhost/sampang/taro_simpeg/common/themes/';
+$themesUrl = 'http://localhost/taro_simpeg/common/themes/';
 $db = 'taro_simpeg';
 //$db = 'taro_simpeg_sampang_new'; //local
 $dbUser = 'root';
-$dbPwd = 'landak';
+$dbPwd = 'bismillah';
 //$dbPwd = ''; //local
 $client = 'simpeg.sampang';
 $clientName = 'Sistem Informasi Pegawai Kab. Sampang';
 $menu = array();
-$rootUrl = 'http://localhost/sampang/taro_simpeg/taro_simpeg/www/'.$client.'/';
+$rootUrl = 'http://localhost/taro_simpeg/taro_simpeg/www/'.$client.'/';
 //===================================================================================
 ?>
