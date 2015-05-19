ca8e56c4e8a2f0edb247dde50c65953865b6d159
x1n73ct <x1n73ct@x1n73ct>
2015-05-19 19:14:14 +0700

    Merge origin/master
    Conflicts:
    	simpeg/views/honorer/index.php

--- simpeg/views/pegawai/_rekapDetail.php
+++ simpeg/views/pegawai/_rekapDetail.php
@@ -1,7 +1,4 @@
-<div style="text-align: right">
-	    <button class="print entypo-icon-printer button" onclick="printDiv('report')" type="button">&nbsp;&nbsp;Print Report</button>    
-	    <a class="btn btn-info pull-right" href="<?php echo url("/pegawai/rekapExcel?".$berdasarkan);?>" target="_blank"><span class="icon16 icomoon-icon-file-excel  white"></span>Export to Excel</a>
-	</div>
+
 	<div class="report" id="report" style="width: 100%; margin-top: 25px;">
 	<h3 style="text-align:center">LAPORAN REKAPITULASI PEGAWAI BERDASARKAN <?php echo $header;?></h3><br>
 	<h6  style="text-align:center">Tangga : <?php echo date('d F Y');?></h6>
