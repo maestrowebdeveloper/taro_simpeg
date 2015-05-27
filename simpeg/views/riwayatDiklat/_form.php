a0a8adf7ab869589e07d6367b7c064ce5f06effe
x1n73ct <x1n73ct@x1n73ct>
2015-05-27 09:39:03 +0700

    Merge origin/master
    Conflicts:
    	simpeg/controllers/PegawaiController.php

---
+++ simpeg/views/riwayatDiklat/_form.php
@@ -0,0 +1,47 @@
+<div class="form">
+    <?php $form=$this->beginWidget('bootstrap.widgets.TbActiveForm',array(
+	'id'=>'riwayat-diklat-form',
+	'enableAjaxValidation'=>false,
+        'method'=>'post',
+	'type'=>'horizontal',
+	'htmlOptions'=>array(
+		'enctype'=>'multipart/form-data'
+	)
+)); ?>
+    <fieldset>
+        <legend>
+            <p class="note">Fields dengan <span class="required">*</span> harus di isi.</p>
+        </legend>
+
+        <?php echo $form->errorSummary($model,'Opps!!!', null,array('class'=>'alert alert-error span12')); ?>
+
+
+                                    <?php echo $form->textFieldRow($model,'pegawai_id',array('class'=>'span5')); ?>
+
+                                        <?php echo $form->textFieldRow($model,'nama',array('class'=>'span5','maxlength'=>255)); ?>
+
+                                        <?php echo $form->textFieldRow($model,'nama_pelatihan',array('class'=>'span5','maxlength'=>255)); ?>
+
+                                        <?php echo $form->textFieldRow($model,'penyelenggara',array('class'=>'span5','maxlength'=>255)); ?>
+
+                                        <?php echo $form->textFieldRow($model,'tahun',array('class'=>'span5','maxlength'=>255)); ?>
+
+                    
+        <?php if (!isset($_GET['v'])) { ?>        <div class="form-actions">
+            <?php $this->widget('bootstrap.widgets.TbButton', array(
+			'buttonType'=>'submit',
+			'type'=>'primary',
+                        'icon'=>'ok white',  
+			'label'=>$model->isNewRecord ? 'Tambah' : 'Simpan',
+		)); ?>
+<?php $this->widget('bootstrap.widgets.TbButton', array(
+			'buttonType'=>'reset',
+                        'icon'=>'remove',  
+			'label'=>'Reset',
+		)); ?>
+        </div>
+        <?php } ?>    </fieldset>
+
+    <?php $this->endWidget(); ?>
+
+</div>
