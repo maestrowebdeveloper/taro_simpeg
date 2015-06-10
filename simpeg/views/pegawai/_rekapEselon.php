888366cc8727239128e3752dc539fd1ce804367c
x1n73ct <x1n73ct@x1n73ct>
2015-06-10 10:29:01 +0700

    Merge origin/master

--- simpeg/views/pegawai/_rekapEselon.php
+++ simpeg/views/pegawai/_rekapEselon.php
@@ -1,17 +1,17 @@
 <div class="report" id="report" style="width: 100%">
     <h3 style="text-align:center">REKAPITULASI DATA ESELON</h3>
-    <h6  style="text-align:center">Tangga : <?php echo date('d F Y'); ?></h6>
+    <h6  style="text-align:center">Tanggal : <?php echo date('d F Y'); ?></h6>
     <hr>
 
     <table class="table table-bordered" border="1">
         <thead>
             <tr>
-                <th style="width:10px">NO</th>
+                <th style="width:20px">NO</th>
                 <th class="span1">NIP</th>
                 <th class="span1">NAMA</th>
-                <th class="span1">GOL</th>					
+                <th class="span1">GOLONGAN</th>					
                 <th class="span1">JABATAN</th>					
-                <th class="span1">ESELON</th>					
+                <th class="span1">UNIT KERJA</th>					
                 <th class="span1">ALAMAT</th>					
             </tr>
         </thead>
@@ -20,15 +20,15 @@
             $no = 1;
             if (!empty($model)) {
                 foreach ($model as $value) {
-                    $eselon = isset($value->JabatanStruktural->Eselon->nama) ? $value->JabatanStruktural->Eselon->nama : "-";
+//                    $eselon = isset($value->JabatanStruktural->Eselon->nama) ? $value->JabatanStruktural->Eselon->nama : "-";
                     echo '	
 		<tr>
 			<td>' . $no . '</td>
-                        <td>' . $value->nip . '</td>	
-			<td>' . $value->nama . '</td>		
-			<td>' . $value->pangkat . '</td>			
+                        <td>' . $value->nip."&nbsp;". '</td>	
+			<td>' . $value->namaGelar . '</td>		
+			<td>' . $value->Pangkat->golongan . '</td>			
 			<td>' . $value->jabatan . '</td>			
-			<td>' . $eselon . '</td>			
+			<td>' . $value->jabatan . '</td>			
 			<td>' . $value->alamat . '</td>			
 									
 		</tr>';
